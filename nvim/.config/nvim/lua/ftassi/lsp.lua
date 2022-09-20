4a39d1bf8e9dda1e77114c70923314e9465f7726
Francesco Tassi <tassi.francesco@gmail.com>
2022-09-20 17:11:03 +0200

    Intelephense global config
    This should be global but defined at project level. I'm not able to make
    it work in the local .nvimrc file though.
    I'm working on a single php project right now, so having a global config
    will not be a big deal.

--- nvim/.config/nvim/lua/ftassi/lsp.lua
+++ nvim/.config/nvim/lua/ftassi/lsp.lua
@@ -17,6 +17,18 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 lsp.intelephense.setup{ 
     capabilities = capabilities,
     on_attach = require("aerial").on_attach,
+    settings = {
+        intelephense = {
+            -- debug = {enable = true},
+            environment = {phpVersion = "7.4"},
+            stubs = {
+              "bcmath", "bz2", "calendar", "Core", "curl", "date", "dba", "dom", "enchant", "fileinfo", "filter", "ftp", "gd", "gettext", "hash", "iconv", "imap", "intl", "json", "ldap", "libxml", "mbstring", "mcrypt", "mysql", "mysqli", "mongo", "mongodb", "password", "pcntl", "pcre", "PDO", "pdo_mysql", "Phar", "readline", "recode", "Reflection", "regex", "session", "SimpleXML", "soap", "sockets", "sodium", "SPL", "standard", "superglobals", "sysvsem", "sysvshm", "tokenizer", "xml", "xdebug", "xmlreader", "xmlwriter", "yaml", "zip", "zlib", "wordpress", "woocommerce", "acf-pro", "wordpress-globals", "wp-cli", "genesis", "polylang",
+            },
+            files = {
+                maxSize = 5000000
+            }
+        },
+    },
 }
 
 lsp.elmls.setup{ 
