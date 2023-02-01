c8a67e27ea0b049f21153c5148d54ec86dae0d2a
Francesco Tassi <tassi.francesco@gmail.com>
2023-02-01 22:25:31 +0100

    Update cmp setup

--- nvim/.config/nvim/lua/ftassi/lsp.lua
+++ nvim/.config/nvim/lua/ftassi/lsp.lua
@@ -1,6 +1,6 @@
 local lsp = require('lspconfig')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- vim.lsp.set_log_level('debug')
 
