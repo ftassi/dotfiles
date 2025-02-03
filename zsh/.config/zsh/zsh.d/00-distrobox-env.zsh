9bb63f1372da9ca18d540687818d49f8b2be6d14
Francesco Tassi <tassi.francesco@gmail.com>
2025-02-04 00:28:50 +0100

    Add nvim nvm configuration

--- zsh/.config/zsh/zsh.d/00-distrobox-env.zsh
+++ zsh/.config/zsh/zsh.d/00-distrobox-env.zsh
@@ -7,4 +7,16 @@ if [ "$CONTAINER_ID" = "nvim" ]; then
     # Puoi aggiungere ulteriori configurazioni specifiche per la distrobox nvim
     # Ad esempio, aggiungi alla PATH la directory dei binari della distrobox:
     export PATH="$HOME/.local/distroboxes/nvim/bin:$PATH"
+
+    # create a directory for nvm if not exists
+    if [ ! -d "$HOME/opt/distroboxes/nvim/.nvm" ]; then
+        mkdir -p "$HOME/opt/distroboxes/nvim/.nvm"
+    fi
+
+    export NVM_DIR="$HOME/opt/distroboxes/nvim/.nvm"
+    if [ -s "$NVM_DIR/nvm.sh" ]; then
+        . "$NVM_DIR/nvm.sh"
+    fi
+    # Puoi anche aggiungere il percorso dei binari di nvm (se necessario) al PATH
+    export PATH="$NVM_DIR/versions/node/$(nvm version)/bin:$PATH"
 fi
