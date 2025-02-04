e2291d521a6c0754cc2fd4d6740afdbde6a98283
Francesco Tassi <tassi.francesco@gmail.com>
2025-02-04 11:21:35 +0100

    Improve nvim distrobox zsh file config
    cargo will be installed in a dedicated dir, hence we customize env
    variables.
    We don't want to create nvim directory here, distrobox provisioning
    should take care of that.

--- zsh/.config/zsh/zsh.d/00-distrobox-env.zsh
+++ zsh/.config/zsh/zsh.d/00-distrobox-env.zsh
@@ -1,26 +1,25 @@
 # Se siamo dentro la distrobox "nvim", regola l'ambiente
-if [[ "$CONTAINER_ID" == "nvim" ]]; then
+if [[ "$CONTAINER_ID" == "$CONTAINER_ID" ]]; then
     # Se esiste il file di configurazione di Cargo, esegui il sourcing
-    if [[ -f "$HOME/.cargo/env" ]]; then
-        source "$HOME/.cargo/env"
-    fi
+    export CARGO_HOME="$HOME/distroboxes/$CONTAINER_ID/cargo"
+    export CARGO_HOME="$HOME/distroboxes/$CONTAINER_ID/rustup"
 
-    # Aggiungi alla PATH la directory dei binari specifica per la distrobox
-    export PATH="$HOME/.local/distroboxes/nvim/bin:$PATH"
-
-    # Crea la directory per nvm se non esiste
-    if [[ ! -d "$HOME/opt/distroboxes/nvim/.nvm" ]]; then
-        mkdir -p "$HOME/opt/distroboxes/nvim/.nvm"
+    if [[ -f "$CARGO_HOME/env" ]]; then
+        source "$CARGO_HOME/env"
     fi
 
     # Imposta NVM_DIR e carica nvm
-    export NVM_DIR="$HOME/opt/distroboxes/nvim/.nvm"
+    export NVM_DIR="$HOME/distroboxes/$CONTAINER_ID/.nvm"
     if [[ -s "$NVM_DIR/nvm.sh" ]]; then
         source "$NVM_DIR/nvm.sh"
     fi
+
+    # Aggiungi alla PATH la directory dei binari specifica per la distrobox
+    export PATH="$HOME/.local/distroboxes/$CONTAINER_ID/bin:$PATH"
 
     # Se nvm è disponibile, aggiungi alla PATH i binari di Node.js installati tramite nvm
     if command -v nvm >/dev/null; then
         export PATH="$NVM_DIR/versions/node/$(nvm version)/bin:$PATH"
     fi
+
 fi
