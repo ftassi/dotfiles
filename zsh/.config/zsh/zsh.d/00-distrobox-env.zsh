3d8cf53153542504af3ba61045840b90a487b1b1
Francesco Tassi <tassi.francesco@gmail.com>
2025-02-10 14:52:49 +0100

    Add dev distrobox zshrc file

--- zsh/.config/zsh/zsh.d/00-distrobox-env.zsh
+++ zsh/.config/zsh/zsh.d/00-distrobox-env.zsh
@@ -24,3 +24,29 @@ if [[ "$CONTAINER_ID" == "nvim" ]]; then
     fi
 
 fi
+
+if [[ "$CONTAINER_ID" == "dev" ]]; then
+    # Se esiste il file di configurazione di Cargo, esegui il sourcing
+    export CARGO_HOME="$HOME/distroboxes/$CONTAINER_ID/cargo"
+    export RUSTUP_HOME="$HOME/distroboxes/$CONTAINER_ID/rustup"
+
+    if [[ -f "$CARGO_HOME/env" ]]; then
+        source "$CARGO_HOME/env"
+    fi
+
+    # Imposta NVM_DIR e carica nvm
+    export NVM_DIR="$HOME/distroboxes/$CONTAINER_ID/.nvm"
+    if [[ -s "$NVM_DIR/nvm.sh" ]]; then
+        source "$NVM_DIR/nvm.sh"
+    fi
+
+    # Aggiungi alla PATH la directory dei binari specifica per la distrobox
+
+    for d in $HOME/distroboxes/$CONTAINER_ID/*; do PATH="$PATH:$d/bin"; done
+
+    # Se nvm è disponibile, aggiungi alla PATH i binari di Node.js installati tramite nvm
+    if command -v nvm >/dev/null; then
+        export PATH="$NVM_DIR/versions/node/$(nvm version)/bin:$PATH"
+    fi
+
+fi
