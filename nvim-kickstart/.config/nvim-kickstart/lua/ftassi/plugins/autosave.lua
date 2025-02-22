66b2858207e940ad8bf5f2ef19e6924d045a333c
Francesco Tassi <tassi.francesco@gmail.com>
2025-02-22 16:34:42 +0100

    Add autosave with a 1000ms debounce delay
    The debounce should avoid code flickering when used in conjunction with
    rustfmt format on save. Not sure about the timing though, this need to
    be tested more.

--- nvim-kickstart/.config/nvim-kickstart/lua/ftassi/plugins/autosave.lua
+++ nvim-kickstart/.config/nvim-kickstart/lua/ftassi/plugins/autosave.lua
@@ -1,7 +1,7 @@
-return {}
--- return {
---   'Pocco81/auto-save.nvim',
---   opts = {
---     trigger_events = { 'InsertLeave', 'BufLeave', 'FocusLost', 'TextChanged' },
---   },
--- }
+return {
+  'Pocco81/auto-save.nvim',
+  opts = {
+    trigger_events = { 'InsertLeave', 'BufLeave', 'FocusLost', 'TextChanged' },
+    debounce_delay = 1000,
+  },
+}
