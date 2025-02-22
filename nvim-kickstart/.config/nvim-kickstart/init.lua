bdf17e4e4c86c7cee3bb86387ec822466d455968
Francesco Tassi <tassi.francesco@gmail.com>
2025-02-22 15:31:07 +0100

    Restore onedark theme

--- nvim-kickstart/.config/nvim-kickstart/init.lua
+++ nvim-kickstart/.config/nvim-kickstart/init.lua
@@ -95,17 +95,22 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   {
-    'folke/tokyonight.nvim',
+    'navarasu/onedark.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'onedark'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
+    opts = { 
+      transparent = true,
+      colors = {
+        white = '#ffffff',
+      },
+      highlights = {
+        LineNr = { fg = '$white'},
+        CursorLineNr = { fg = '$yellow', fmt = 'bold' },
+      },
+    },
   },
 
   -- require 'kickstart.plugins.debug',
