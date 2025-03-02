dbd89e51e93d76bcb0cde8cae57262aa0d2e9487
Francesco Tassi <tassi.francesco@gmail.com>
2025-03-03 00:51:22 +0100

    Copilot config

--- nvim/.config/nvim/lua/ftassi/plugins/00_base.lua
+++ nvim/.config/nvim/lua/ftassi/plugins/00_base.lua
@@ -14,9 +14,44 @@ return {
   {
     'zbirenbaum/copilot.lua',
     cmd = 'Copilot',
-    event = 'InsertEnter',
+    event = { 'InsertEnter', 'CmdlineEnter', 'BufReadPost' },
     config = function()
-      require('copilot').setup {}
+      require('copilot').setup {
+        panel = {
+          enabled = false,
+          -- auto_refresh = true,
+          -- keymap = {
+          --   accept = '<C-j>',
+          --   accept_word = '<C-k>',
+          --   accept_line = '<C-l>',
+          --   next = '<C-n>',
+          --   prev = '<C-d>',
+          --   dismiss = '<C-f>',
+          -- },
+          -- layout = {
+          --   position = 'bottom', -- | top | left | right
+          --   ratio = 0.4,
+          -- },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = '<C-j>',
+            accept_word = '<C-k>',
+            accept_line = '<C-l>',
+            next = '<C-n>',
+            prev = '<C-d>',
+            dismiss = '<C-f>',
+          },
+        },
+        filetypes = {
+          ['*'] = true,
+        },
+        copilot_node_command = 'node', -- Node.js version must be > 16.x
+        server_opts_overrides = {},
+      }
     end,
   },
   { 'AndrewRadev/tagalong.vim' },
