d896704a65cf8fdcdf13a555ab4e73f3d2a2ba2a
Francesco Tassi <tassi.francesco@gmail.com>
2025-03-05 20:57:34 +0100

    Reformat

--- nvim/.config/nvim/lua/ftassi/plugins/test.lua
+++ nvim/.config/nvim/lua/ftassi/plugins/test.lua
@@ -1,13 +1,13 @@
 return {
-  "vim-test/vim-test",
-  event = "VeryLazy",  -- o l'evento che preferisci
+  'vim-test/vim-test',
+  event = 'VeryLazy', -- o l'evento che preferisci
   config = function()
     -- Imposta le variabili globali richieste da vim-test:
-    vim.g["test#strategy"] = "neovim"
-    vim.g["test#neovim#term_position"] = "vert botright"
-    vim.g["test#rust#cargotest#test_options"] = "-- --nocapture"
-    
+    vim.g['test#strategy'] = 'neovim'
+    vim.g['test#neovim#term_position'] = 'vert botright'
+    vim.g['test#rust#cargotest#test_options'] = '-- --nocapture'
+
     -- Richiama il modulo dei keymaps per vim-test
-    require("ftassi.keymaps").test()
+    require('ftassi.keymaps').test()
   end,
 }
