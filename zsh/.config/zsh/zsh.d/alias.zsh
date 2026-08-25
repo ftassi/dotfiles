62001563119d8990aa8e1d301c9faaeab5c54b75
Francesco Tassi <tassi.francesco@gmail.com>
2026-08-25 23:28:04 +0200

    Add dotfiles alias to open the repo in nvim

--- zsh/.config/zsh/zsh.d/alias.zsh
+++ zsh/.config/zsh/zsh.d/alias.zsh
@@ -4,3 +4,4 @@ alias pbpaste='xclip -selection clipboard -o'
 # Add an alias to test a new configuration from ~/config/nvim-kickstart
 # alias vim='NVIM_APPNAME="nvim-kickstart" nvim'
 alias vim-bak='NVIM_APPNAME="nvim-bak" nvim'
+alias dotfiles='cd ~/dotfiles && nvim'
